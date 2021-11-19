01e9bb0e684d9f192bceda315c83ff8b6e6ea0a3
Shubham <71990977+Skparab1@users.noreply.github.com>
2021-11-18 16:42:21 -0800

    Create sender.svelte

---
+++ src/sender.svelte
@@ -0,0 +1,29 @@
+<script>
+    import { peer, conn, messages} from './store.js';
+    $: messages_value =[];
+    messages.subscribe(value => {
+        messages_value = value;
+    })
+    let msg;
+    let conn_value;
+    conn.subscribe(value => {
+        conn_value = value;
+    })
+    peer.on('connection', function(connection) {
+        connection.on("data", (msg)=>{
+            messages.set([...messages_value, msg])
+        })
+        console.log("fdfdfd");
+    })
+    function sendMsg() {
+        conn_value.send(msg)
+        messages.set([...messages_value, msg])
+    }
+</script>
+
+{#each messages_value as peerMessage}
+    <p>{peerMessage}</p>
+
+{/each}
+<input type="text" bind:value={msg}>
+<button on:click={sendMsg(msg)}>Send</button>

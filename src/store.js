1dfbcd260cc41f593a94ba0441fb685062c12102
Shubham <71990977+Skparab1@users.noreply.github.com>
2021-11-18 18:11:21 -0800

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,5 +1,3 @@
 import { writable } from 'svelte/store';
-import randomWords from 'random-words';
-export const peer = new window.Peer(randomWords());
 export const conn = writable("");
 export const messages = writable([]);
